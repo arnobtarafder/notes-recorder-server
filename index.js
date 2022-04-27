12dd60a8e37496f3742fbf74687e2ab5d5c8a280
arnobtarafder <rmtarnob@gmail.com>
2022-04-27 22:34:42 +0600

    implement post functionality

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const ObjectId = require("mongodb").ObjectId;
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 4000;
 require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-// notetaker
-// Pj9NNfDgHIZlka7B
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n63qw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -37,53 +35,47 @@ async function run() {
         });
 
 
-        // create notesTaker
+        // create a note using post method
         app.post("/note", async (req, res) => {
             const data = req.body;
-            console.log(data);
-            const result = await notesCollection.insertOne();
+            // console.log("from post api", data);
+            const result = await notesCollection.insertOne(data);
             res.send(result)
         });
 
 
-        // update notesTaker
+        // update a note using put method
         app.put("/note/:id", async (req, res) => {
-            const data = req.body;
             const id = req.params.id;
+            const data = req.body;
+            console.log(data);
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
-
             const updateDoc = {
-                $set: {
-                    // username: data.userName, 
-                    // textData: data.textData,
-                    ...data
-                },
-                // $set: req.body
+              $set: {
+                ...data,
+              },
             };
-
-            const result = await notesCollection.updateOne(filter, updateDoc, options);
-
+            const result = await notesCollection.updateOne(
+              filter,
+              updateDoc,
+              options
+            );
             res.send(result);
-
-        });
-
-
-        // delete note
-        app.delete("/note/:id", async(req,res) => {
+          });
+      
+          // delete a note using delete method
+          // http://localhost:5000/note/62642d6e15e58fa28951096e
+          app.delete("/note/:id", async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const result = await notesCollection.deleteOne(filter);
-
             res.send(result);
-        })
-
-        console.log("connected to database");
-    }
-    finally {
-
-    }
-}
+          });
+          console.log("connected to db");
+        } finally {
+        }
+      }
 run().catch(console.dir)
 
 
